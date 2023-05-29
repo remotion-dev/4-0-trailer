214a9909c790c44d06ded157e1a484ab76726811
Jonny Burger <jonathanburger11@gmail.com>
2023-05-29 14:44:48 +0200

    restore button

--- src/RenderButton.tsx
+++ src/RenderButton.tsx
@@ -1,35 +1,29 @@
-import {parsePath, resetPath, scalePath} from '@remotion/paths';
-import {getBoundingBox} from '@remotion/paths';
+import {
+	getBoundingBox,
+	parsePath,
+	resetPath,
+	scalePath,
+	translatePath,
+} from '@remotion/paths';
 import {makeRect} from '@remotion/shapes';
 import React from 'react';
-import {
-	AbsoluteFill,
-	Sequence,
-	spring,
-	useCurrentFrame,
-	useVideoConfig,
-} from 'remotion';
+import {AbsoluteFill, useCurrentFrame} from 'remotion';
 import {getCamera} from './camera';
-import {CursorWithButton} from './CursorAnimation';
 import {Faces} from './Faces';
 import {turnInto3D} from './fix-z';
 import {useText} from './get-char';
 import {extrudeInstructions} from './join-inbetween-tiles';
-import {FaceType, translateSvgInstruction} from './map-face';
+import {FaceType, transformFace, translateSvgInstruction} from './map-face';
 import {rotated, translated} from './matrix';
 import {projectPoints} from './project-points';
-import {Sparks} from './Sparks';
 import {subdivideInstructions} from './subdivide-instruction';
 
-const viewBox = [-1600, -800, 3000, 1600];
-
-const maxDepth = 20;
+const viewBox = [-1600, -800, 3200, 1600];
 
 const buttonColor = '#0b84f3';
 
 export const RenderButton: React.FC = () => {
 	const frame = useCurrentFrame();
-	const {fps} = useVideoConfig();
 	const shape = makeRect({
 		height: 60,
 		width: 200,
@@ -37,27 +31,18 @@ export const RenderButton: React.FC = () => {
 	});
 
 	const bBox = getBoundingBox(shape.path);
-	const parsed = subdivideInstructions(
-		subdivideInstructions(turnInto3D(parsePath(shape.path)))
+	const buttonWidth = bBox.x2 - bBox.x1;
+	const buttonHeight = bBox.y2 - bBox.y1;
+
+	const centeredButton = translatePath(
+		shape.path,
+		-buttonWidth / 2,
+		-buttonHeight / 2
 	);
 
-	const press =
-		spring({
-			fps,
-			frame: frame - 60,
-			config: {
-				damping: 200,
-			},
-			durationInFrames: 10,
-		}) -
-		spring({
-			fps,
-			frame: frame - 70,
-			config: {
-				damping: 200,
-			},
-			durationInFrames: 10,
-		});
+	const parsed = subdivideInstructions(
+		subdivideInstructions(turnInto3D(parsePath(centeredButton)))
+	);
 
 	const text = useText('Render video');
 	if (!text) {
@@ -65,16 +50,9 @@ export const RenderButton: React.FC = () => {
 	}
 	const textPath = resetPath(scalePath(text.path, 0.25, 0.25));
 	const parsedText = parsePath(textPath);
-	const depth = maxDepth - press * maxDepth;
+	const depth = 30;
 
-	const threeD = turnInto3D(parsedText).map((p) => {
-		return translateSvgInstruction(p, 0, 0, -depth / 2);
-	});
-
-	const width = bBox.x2 - bBox.x1;
-	const height = bBox.y2 - bBox.y1;
-
-	const inbetweenFaces: FaceType[] = extrudeInstructions({
+	const extrudedButton: FaceType[] = extrudeInstructions({
 		instructions: {
 			points: parsed,
 			color: buttonColor,
@@ -84,39 +62,34 @@ export const RenderButton: React.FC = () => {
 		},
 		depth,
 		sideColor: 'black',
-		frontFaceColor: 'red',
-		backFaceColor: 'green',
+		frontFaceColor: '#0b84f3',
+		backFaceColor: 'black',
 		drawSegmentLines: false,
-	});
-
-	const transformations = [rotated([0, 1, 0], frame / 10)];
-
-	const rotatedFaces = inbetweenFaces.map((face) => {
-		return projectPoints({
-			transformations,
-			face,
-		});
 	});
 
 	const bBoxText = getBoundingBox(textPath);
 
-	const textProjected = projectPoints({
-		transformations: [
-			translated([
-				-(bBoxText.x2 - bBoxText.x1) / 2,
-				-(bBoxText.y2 - bBoxText.y1) / 2,
-				0,
-			]),
-			rotated([0, 1, 0], frame / 10),
-		],
-		face: {
-			centerPoint: [0, 0, -depth / 2, 1],
+	const centeredText = turnInto3D(parsedText).map((turn) => {
+		return translateSvgInstruction(
+			turn,
+			-(bBoxText.x2 - bBoxText.x1) / 2,
+			-(bBoxText.y2 - bBoxText.y1) / 2,
+			0
+		);
+	});
+
+	const textFace = transformFace(
+		{
+			centerPoint: [0, 0, 0, 1],
 			color: 'white',
 			isStroke: false,
-			points: threeD,
+			points: centeredText,
 			shouldDrawLine: false,
 		},
-	});
+		[translated([0, 0, -depth / 2 - 0.001])]
+	);
+
+	const transformations = [rotated([0, 1, 0], frame / 10)];
 
 	return (
 		<AbsoluteFill>
@@ -129,15 +102,16 @@ export const RenderButton: React.FC = () => {
 			>
 				<svg viewBox={viewBox.join(' ')} style={{overflow: 'visible'}}>
 					<Faces
-						camera={getCamera(width, height)}
-						faces={[...rotatedFaces, textProjected]}
+						camera={getCamera(buttonWidth, buttonHeight)}
+						faces={[...extrudedButton, textFace].map((face) => {
+							return projectPoints({
+								transformations,
+								face,
+							});
+						})}
 					/>
 				</svg>
 			</AbsoluteFill>
-			<CursorWithButton />
-			<Sequence from={80}>
-				<Sparks />
-			</Sequence>
 		</AbsoluteFill>
 	);
 };
