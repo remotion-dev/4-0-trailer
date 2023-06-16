e1b4074757bd4d2bbda49ad88992986a9487fbac
Jonny Burger <jonathanburger11@gmail.com>
2023-06-16 14:26:11 +0200

    Update Faces.tsx

--- src/Faces.tsx
+++ src/Faces.tsx
@@ -1,8 +1,6 @@
 import React, {useMemo} from 'react';
-import {cameraEye} from './camera';
 import {ThreeDElement} from './element';
 import {Face} from './FaceComp';
-import {rayTracing} from './ray-tracing';
 
 export const Faces: React.FC<{
 	elements: ThreeDElement[];
@@ -17,29 +15,6 @@ export const Faces: React.FC<{
 
 	const sortedFaces = useMemo(() => {
 		return faces.slice().sort((a, b) => {
-			const rayTracedPoint = rayTracing({
-				camera: cameraEye,
-				firstPlaneCorner: a.points[0].point,
-				secondPlaneNormal: b.normal,
-				secondPlanePoint: b.points[0].point,
-			});
-
-			if (rayTracedPoint.type === 'parallel') {
-				console.log('parallel');
-				return a.centerPoint[2] - b.centerPoint[2];
-			}
-
-			for (const point of a.points) {
-				const diff = point.point[2] - rayTracedPoint.point[2];
-
-				if (diff === 0) {
-					continue;
-				}
-
-				return diff;
-			}
-
-			console.log({a, b});
 			return a.centerPoint[2] - b.centerPoint[2];
 		});
 	}, [faces]);
