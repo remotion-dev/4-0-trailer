f163e882ace59adc6f11b77c86409db7017b62a6
Jonny Burger <jonathanburger11@gmail.com>
2023-06-30 21:04:41 +0200

    folder

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import {Composition} from 'remotion';
+import {Composition, Folder} from 'remotion';
 import {TriangleOut} from './3DRemotionLogo';
 import {AudioViz} from './AudioViz';
 import {DiscoverRemotion4} from './DiscoverRemotion4';
@@ -15,7 +15,6 @@ import {RenderButton} from './RenderButton';
 import {RenderProgress} from './RenderProgress';
 import {RightPaneLogo} from './RightPaneLogo';
 import {RustLogo} from './RustLogo';
-import {Sparks} from './Sparks';
 import {Spiral} from './Spiral';
 import {Studio} from './Studio';
 import {Teaser} from './Teaser';
@@ -26,217 +25,219 @@ import {WaysToRender} from './WaysToRender';
 export const RemotionRoot: React.FC = () => {
 	return (
 		<>
-			<Composition
-				id="RenderButton"
-				component={RenderButton}
-				durationInFrames={600}
-				fps={30}
-				width={1280}
-				height={720}
-			/>
-			<Composition
-				id="Stars"
-				component={Sparks}
-				durationInFrames={200}
-				fps={30}
-				width={1280}
-				height={720}
-			/>
-			<Composition
-				id="Logo"
-				component={TriangleOut}
-				durationInFrames={1200}
-				fps={30}
-				width={1280}
-				height={720}
-				defaultProps={{
-					background: 'white',
-					dark: false,
-				}}
-			/>
-			<Composition
-				id="RenderProgress"
-				component={RenderProgress}
-				durationInFrames={1200}
-				fps={30}
-				width={1280}
-				height={720}
-			/>
-			<Composition
-				id="AudioViz"
-				component={AudioViz}
-				durationInFrames={1400}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Main"
-				component={Main}
-				durationInFrames={1900}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="NumberedChapter"
-				component={Cube}
-				durationInFrames={60}
-				fps={30}
-				width={1920}
-				height={1080}
-				defaultProps={{label: '@remotion/tailwind', step: 1}}
-				schema={cubeSchema}
-			/>
-			<Composition
-				id="Timeline"
-				component={TimelinePerspective1}
-				durationInFrames={600}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Timeline2"
-				component={TimelinePerspective2}
-				durationInFrames={600}
-				fps={30}
-				width={1920}
-				height={1080}
-				defaultProps={{
-					withAudio: true,
-				}}
-			/>
-			<Composition
-				id="NpmInitVideo"
-				component={NpmIniVideo}
-				durationInFrames={600}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="ZodSupport"
-				component={RightPaneLogo}
-				durationInFrames={600}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Teaser"
-				component={Teaser}
-				durationInFrames={15.4 * 30}
-				fps={30}
-				width={1080}
-				height={1080}
-			/>
-			<Composition
-				id="DistinctlyReact"
-				component={DistinctlyReact}
-				durationInFrames={Math.floor(22 * 25 + 44 / 100)}
-				fps={25}
-				width={1920 * 2}
-				height={1080 * 2}
-			/>
-			<Composition
-				id="WaysToRender"
-				component={WaysToRender}
-				durationInFrames={600}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="RustLogo"
-				component={RustLogo}
-				durationInFrames={44.68 * 25}
-				fps={25}
-				width={1920 * 2}
-				height={1080 * 2}
-			/>
-			<Composition
-				id="Improvement"
-				component={Reencoding}
-				durationInFrames={12 * 25}
-				fps={25}
-				width={1920 * 2}
-				height={1080 * 2}
-			/>
-			<Composition
-				id="FeatureMap"
-				component={FeatureMap}
-				durationInFrames={12 * 25}
-				fps={25}
-				width={1920 * 2}
-				height={1080 * 2}
-			/>
-			<Composition
-				id="OgImage"
-				component={OgImage}
-				durationInFrames={12 * 25}
-				fps={25}
-				width={1920 * 2}
-				height={1080 * 2}
-				schema={ogImageSchema}
-				defaultProps={{
-					dark: true,
-				}}
-			/>
-			<Composition
-				id="InFrameLogo"
-				component={InFrameLogo}
-				durationInFrames={100000}
-				fps={25}
-				width={1920 * 2}
-				height={1080 * 1.5}
-				defaultProps={{
-					background: 'transparent',
-				}}
-			/>
-			<Composition
-				id="TeaserWheel"
-				component={Wheel}
-				durationInFrames={100000}
-				fps={25}
-				width={1080}
-				height={1080}
-				defaultProps={{
-					topLayer: true,
-				}}
-			/>
-			<Composition
-				id="Everything"
-				component={Everything}
-				durationInFrames={300}
-				fps={25}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Spiral"
-				component={Spiral}
-				durationInFrames={1200}
-				fps={25}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Studio"
-				component={Studio}
-				durationInFrames={1200}
-				fps={25}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Discover"
-				component={DiscoverRemotion4}
-				durationInFrames={1200}
-				fps={25}
-				width={1920}
-				height={1080}
-			/>
+			<Folder name="Intro">
+				<Composition
+					id="Intro"
+					component={Main}
+					durationInFrames={1900}
+					fps={30}
+					width={1920}
+					height={1080}
+				/>
+			</Folder>
+			<Folder name="IntroScenes">
+				<Composition
+					id="1-Logo"
+					component={TriangleOut}
+					durationInFrames={1200}
+					fps={30}
+					width={1920}
+					height={1080}
+					defaultProps={{
+						background: 'white',
+						dark: false,
+					}}
+				/>
+				<Composition
+					id="2-NpmInitVideo"
+					component={NpmIniVideo}
+					durationInFrames={600}
+					fps={30}
+					width={1920}
+					height={1080}
+				/>
+				<Composition
+					id="3-Timeline"
+					component={TimelinePerspective1}
+					durationInFrames={600}
+					fps={30}
+					width={1920}
+					height={1080}
+				/>
+				<Composition
+					id="4-RenderButton"
+					component={RenderButton}
+					durationInFrames={600}
+					fps={30}
+					width={1280}
+					height={720}
+				/>
+				<Composition
+					id="5-RenderProgress"
+					component={RenderProgress}
+					durationInFrames={1200}
+					fps={30}
+					width={1280}
+					height={720}
+				/>
+				<Composition
+					id="6-Spiral"
+					component={Spiral}
+					durationInFrames={1200}
+					fps={25}
+					width={1920}
+					height={1080}
+				/>
+				<Composition
+					id="7-AudioTimeline"
+					component={TimelinePerspective2}
+					durationInFrames={600}
+					fps={30}
+					width={1920}
+					height={1080}
+					defaultProps={{
+						withAudio: true,
+					}}
+				/>
+				<Composition
+					id="8-Studio"
+					component={Studio}
+					durationInFrames={1200}
+					fps={25}
+					width={1920}
+					height={1080}
+				/>
+			</Folder>
+			<Folder name="Throwaways">
+				<Composition
+					id="AudioViz"
+					component={AudioViz}
+					durationInFrames={1400}
+					fps={30}
+					width={1920}
+					height={1080}
+				/>
+				<Composition
+					id="NumberedChapter"
+					component={Cube}
+					durationInFrames={60}
+					fps={30}
+					width={1920}
+					height={1080}
+					defaultProps={{label: '@remotion/tailwind', step: 1}}
+					schema={cubeSchema}
+				/>
+				<Composition
+					id="Everything"
+					component={Everything}
+					durationInFrames={300}
+					fps={25}
+					width={1920}
+					height={1080}
+				/>
+			</Folder>
+			<Folder name="KeynoteOverlays">
+				<Composition
+					id="RustLogo"
+					component={RustLogo}
+					durationInFrames={44.68 * 25}
+					fps={25}
+					width={1920 * 2}
+					height={1080 * 2}
+				/>
+				<Composition
+					id="ZodSupport"
+					component={RightPaneLogo}
+					durationInFrames={600}
+					fps={30}
+					width={1920}
+					height={1080}
+				/>
+				<Composition
+					id="DistinctlyReact"
+					component={DistinctlyReact}
+					durationInFrames={Math.floor(22 * 25 + 44 / 100)}
+					fps={25}
+					width={1920 * 2}
+					height={1080 * 2}
+				/>
+				<Composition
+					id="WaysToRender"
+					component={WaysToRender}
+					durationInFrames={600}
+					fps={30}
+					width={1920}
+					height={1080}
+				/>
+				<Composition
+					id="Improvement"
+					component={Reencoding}
+					durationInFrames={12 * 25}
+					fps={25}
+					width={1920 * 2}
+					height={1080 * 2}
+				/>
+				<Composition
+					id="FeatureMap"
+					component={FeatureMap}
+					durationInFrames={12 * 25}
+					fps={25}
+					width={1920 * 2}
+					height={1080 * 2}
+				/>
+				<Composition
+					id="Discover"
+					component={DiscoverRemotion4}
+					durationInFrames={1200}
+					fps={25}
+					width={1920}
+					height={1080}
+				/>
+			</Folder>
+			<Folder name="SocialMediaAssets">
+				<Composition
+					id="Teaser"
+					component={Teaser}
+					durationInFrames={15.4 * 30}
+					fps={30}
+					width={1080}
+					height={1080}
+				/>
+				<Composition
+					id="OgImage"
+					component={OgImage}
+					durationInFrames={12 * 25}
+					fps={25}
+					width={1920 * 2}
+					height={1080 * 2}
+					schema={ogImageSchema}
+					defaultProps={{
+						dark: true,
+					}}
+				/>
+				<Composition
+					id="InFrameLogo"
+					component={InFrameLogo}
+					durationInFrames={100000}
+					fps={25}
+					width={1920 * 2}
+					height={1080 * 1.5}
+					defaultProps={{
+						background: 'transparent',
+					}}
+				/>
+				<Composition
+					id="TeaserWheel"
+					component={Wheel}
+					durationInFrames={100000}
+					fps={25}
+					width={1080}
+					height={1080}
+					defaultProps={{
+						topLayer: true,
+					}}
+				/>
+			</Folder>
 		</>
 	);
 };
